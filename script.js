c018f6680675dab0e6bbb9bd3f4542ec998bab46
Claudia Petty <claudialove32@gmail.com>
2020-06-16 17:57:00 -0400

    add stylesheet and reined js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var guess = prompt("Try and guess the number i'm thinking between 1 and 10");
 while (Number(guess) !== secretNum) {
    
     //too high
-    if (Number(guess) > secretNum && Number(guess) < 10) {
+    if (Number(guess) > secretNum && Number(guess) <= 10) {
         alert ("Too high, guess again...")
     }
     //too low
@@ -21,6 +21,9 @@ while (Number(guess) !== secretNum) {
     else if (Number(guess) > 10 ) {
        alert ("that number is NOT between 1-10");
     }
+    else if (Number(guess) = (null) ) {
+        alert ("YOU CAN'T LEAVE IT BLANK!!");
+     }
     var guess = prompt("Try and guess the number i'm thinking between 1 and 10");
 };
     //correct guess
