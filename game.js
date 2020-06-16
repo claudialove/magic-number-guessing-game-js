d2a206fc082f6c206947e40c6bedc054a5ee2053
Claudia Petty <claudialove32@gmail.com>
2020-06-16 17:17:37 -0400

    mulit-decision-while-loop

---
+++ game.js
@@ -0,0 +1,29 @@
+
+//create number
+var secretNum = 5;
+
+//prompt user to guess
+var guess = prompt("Try and guess the number i'm thinking between 1 and 10");
+
+
+//while loop instead
+while (Number(guess) !== secretNum) {
+   
+    //too high
+    if (Number(guess) > secretNum && Number(guess) < 10) {
+        alert ("Too high, guess again...")
+    }
+    //too low
+   else if (Number(guess) < secretNum) {
+        alert ("Too low, guess again...")
+    }
+    //out of bounds
+    else if (Number(guess) > 10 ) {
+       alert ("that number is NOT between 1-10");
+    }
+    var guess = prompt("Try and guess the number i'm thinking between 1 and 10");
+};
+    //correct guess
+    alert("YOU WIN!!!!!")
+
+ 
