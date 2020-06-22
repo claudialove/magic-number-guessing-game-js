0487257e1fe37fac0b3e26bfd0952086a7b48447
Claudia Petty <claudialove32@gmail.com>
2020-06-22 07:33:35 -0400

    adding window.setTimeout function

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 
-//create number
-var secretNum = 5;
+window.setTimeout(function(){
 
-//prompt user to guess
-var guess = prompt("Try and guess the number i'm thinking between 1 and 10");
+    function playGame()  {
+
+        //create number
+        var secretNum = 5;
+        
+        //prompt user to guess
+        var guess = prompt("Try and guess the number i'm thinking between 1 and 10");
+        
+        
+        //while loop instead
+        while (Number(guess) !== secretNum) {
+           
+            //too high
+            if (Number(guess) > secretNum && Number(guess) <= 10) {
+                alert ("Too high, guess again...")
+            }
+            //too low
+           else if (Number(guess) < secretNum) {
+                alert ("Too low, guess again...")
+            }
+            //out of bounds
+            else if (Number(guess) > 10 ) {
+               alert ("that number is NOT between 1-10");
+            }
+            else if (Number(guess) = (null) ) {
+                alert ("YOU CAN'T LEAVE IT BLANK!!");
+             }
+            var guess = prompt("Try and guess the number i'm thinking between 1 and 10");
+        };
+            //correct guess
+            alert("YOU WIN!!!!!")
+        
+        }
+        
+        playGame();
 
 
-//while loop instead
-while (Number(guess) !== secretNum) {
-   
-    //too high
-    if (Number(guess) > secretNum && Number(guess) <= 10) {
-        alert ("Too high, guess again...")
-    }
-    //too low
-   else if (Number(guess) < secretNum) {
-        alert ("Too low, guess again...")
-    }
-    //out of bounds
-    else if (Number(guess) > 10 ) {
-       alert ("that number is NOT between 1-10");
-    }
-    else if (Number(guess) = (null) ) {
-        alert ("YOU CAN'T LEAVE IT BLANK!!");
-     }
-    var guess = prompt("Try and guess the number i'm thinking between 1 and 10");
-};
-    //correct guess
-    alert("YOU WIN!!!!!")
+}, 500);
 
- 
+
